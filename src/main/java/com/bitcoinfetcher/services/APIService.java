559ef472f2678296b1634eee235d95a05adb86cd
Mia Jindrichova <jindrich.lucie@gmail.com>
2021-06-10 17:16:18 +0200

    linear regression prediction

--- src/main/java/com/bitcoinfetcher/services/APIService.java
+++ src/main/java/com/bitcoinfetcher/services/APIService.java
@@ -1,20 +1,16 @@
 package com.bitcoinfetcher.services;
 
 import com.bitcoinfetcher.DTOs.PriceEntryDTO;
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
-import org.apache.commons.math3.stat.regression.SimpleRegression;
 import org.jfree.data.time.Day;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
-import org.jfree.data.xy.XYDataset;
 import org.jfree.ui.RefineryUtilities;
 import org.springframework.http.HttpEntity;
 import org.json.JSONObject;
@@ -42,12 +38,12 @@ public class APIService {
     HttpHeaders headers = new HttpHeaders();
     headers.add(HttpHeaders.ACCEPT, "application/json");
 
-    LocalDateTime localDateTime = LocalDateTime.now();
+    LocalDate localDate = LocalDate.now();
 
     String url = baseURL + "/v1/bpi/historical/close.json";
     UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url);
-    builder.queryParam("start", localDateTime.minusDays(7).toLocalDate().toString());
-    builder.queryParam("end", localDateTime.toLocalDate().toString());
+    builder.queryParam("start", localDate.minusDays(7).toString());
+    builder.queryParam("end", localDate.toString());
 
     HttpEntity request = new HttpEntity(headers);
 
@@ -68,12 +64,7 @@ public class APIService {
       arrayEntries.add(entry);
     }
 
-    // print linear regression prediction
-    SimpleRegression linearRegressionModel = getLinearRegressionModel(arrayEntries);
-    System.out.println(makePrediction(linearRegressionModel, arrayEntries,7));
-
-    //draw linear regression
-    XYDataset dataset = createDataset(arrayEntries);
+    TimeSeriesCollection dataset = createDataset(arrayEntries);
     DrawChart chart = new DrawChart(dataset);
     chart.pack();
     RefineryUtilities.centerFrameOnScreen(chart);
@@ -83,7 +74,7 @@ public class APIService {
     return arrayEntries;
   }
 
-  public XYDataset createDataset(List<PriceEntryDTO> array) {
+  public TimeSeriesCollection createDataset(List<PriceEntryDTO> array) {
     TimeSeriesCollection dataset = new TimeSeriesCollection();
     TimeSeries series = new TimeSeries("Bitcoin Price (in USD)");
 
@@ -96,50 +87,4 @@ public class APIService {
 
     return dataset;
   }
-
-  public SimpleRegression getLinearRegressionModel(List<PriceEntryDTO> array) {
-    SimpleRegression sr = new SimpleRegression();
-
-    for (int i = 0; i < array.size(); i++) {
-      Date date = array.get(i).getDate();
-      DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-      String strDate = dateFormat.format(date);
-      double x = Double.parseDouble(strDate.substring(strDate.length()-2));
-      double y = array.get(i).getPrice();
-      sr.addData(x, y);
-    }
-    return sr;
-  }
-
-  public String makePrediction(SimpleRegression sr, List<PriceEntryDTO> array, int runs) {
-    StringBuilder sb = new StringBuilder();
-    // Display the intercept of the regression
-    sb.append("Intercept: " + sr.getIntercept());
-    sb.append("\n");
-    // Display the slope of the regression.
-    sb.append("Slope: " + sr.getSlope());
-    sb.append("\n");
-    // Display the slope standard error
-    sb.append("Standard Error: " + sr.getSlopeStdErr());
-    sb.append("\n");
-    // Display adjusted R2 value
-    sb.append("Adjusted R2 value: " + sr.getRSquare());
-    sb.append("\n");
-    sb.append("*************************************************");
-    sb.append("\n");
-    sb.append("Running predictions......");
-    sb.append("\n");
-
-    for (int i = 0 ; i < runs ; i++) {
-      Date date = array.get(6).getDate();
-      DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-      String strDate = dateFormat.format(date);
-      int rn = Integer.parseInt(strDate.substring(8))+i+1;
-      sb.append("Day: " + rn + "  Predicted bitcoin price: " + Math.round(sr.predict(rn)));
-      sb.append("\n");
-    }
-    return sb.toString();
-  }
-
-
 }
--- src/main/java/com/bitcoinfetcher/services/DrawChart.java
+++ src/main/java/com/bitcoinfetcher/services/DrawChart.java
@@ -2,15 +2,19 @@ package com.bitcoinfetcher.services;
 
 import java.awt.Color;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.function.LineFunction2D;
 import org.jfree.data.general.DatasetUtilities;
 import org.jfree.data.statistics.Regression;
+import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.ui.ApplicationFrame;
 
@@ -18,46 +22,43 @@ public class DrawChart extends ApplicationFrame {
 
   private static final long serialVersionUID = 1L;
 
-  XYDataset inputData;
+  TimeSeriesCollection inputData;
   JFreeChart chart;
 
-  public DrawChart(XYDataset dataset) {
+  public DrawChart(TimeSeriesCollection dataset) {
     super("Linear Regression");
     inputData = dataset;
     this.chart = createChart(inputData);
 
     ChartPanel chartPanel = new ChartPanel(chart);
-    chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+    chartPanel.setPreferredSize(new java.awt.Dimension(900, 600));
     setContentPane(chartPanel);
   }
 
-  public static JFreeChart createChart(XYDataset inputData) {
+  public JFreeChart createChart(TimeSeriesCollection inputData) {
     // Create the chart using the data read from the prices.txt file
-    JFreeChart chart = ChartFactory.createScatterPlot(
-        "Bitcoin Price Prediction", "Day", "Price", inputData,
-        PlotOrientation.VERTICAL, true, true, false);
+    JFreeChart chart = ChartFactory.createTimeSeriesChart(
+        "Bitcoin Price Prediction", "Day", "Price", inputData, true, true, false);
 
     XYPlot plot = chart.getXYPlot();
+    DateAxis axis = (DateAxis) plot.getDomainAxis();
+    axis.setDateFormatOverride(new SimpleDateFormat("yyyy-MM-dd"));
     plot.getRenderer().setSeriesPaint(0, Color.blue);
     return chart;
   }
 
   public void drawRegressionLine() {
-    // Get the parameters 'a' and 'b' for an equation y = a + b * x,
-    // fitted to the inputData using ordinary least squares regression.
-    // a - regressionParameters[0], b - regressionParameters[1]
-    double regressionParameters[] = Regression.getOLSRegression(inputData,
-        0);
-
-    // Prepare a line function using the found parameters
+    double regressionParameters[] = Regression.getOLSRegression(inputData,0);
     LineFunction2D linefunction2d = new LineFunction2D(
         regressionParameters[0], regressionParameters[1]);
 
-    // Creates a dataset by taking sample values from the line function
+    LocalDate startDate = LocalDate.now().minusDays(7);
+    LocalDate endDate = LocalDate.now().plusDays(3);
     XYDataset dataset = DatasetUtilities.sampleFunction2D(linefunction2d,
-        1D, 15, 100, "Regression Line");
+        startDate.atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli(),
+        endDate.atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli(),
+        100, "Regression Line");
 
-    // Draw the line dataset
     XYPlot xyplot = chart.getXYPlot();
     xyplot.setDataset(1, dataset);
     XYLineAndShapeRenderer xylineandshaperenderer = new XYLineAndShapeRenderer(
@@ -65,5 +66,4 @@ public class DrawChart extends ApplicationFrame {
     xylineandshaperenderer.setSeriesPaint(0, Color.YELLOW);
     xyplot.setRenderer(1, xylineandshaperenderer);
   }
-
 }
